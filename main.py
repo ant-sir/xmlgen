6a2158a966306ef8b64b59f27b2e450a41482d46
朱艳雷10194116 <zhu.yanlei@zte.com.cn>
2018-01-05 14:31:19 +0800

    release 0.9

--- main.py
+++ main.py
@@ -5,21 +5,12 @@ from PyQt5 import QtWidgets
 from frame import Ui_Form
 from PyQt5.QtWidgets import QFileDialog, QMessageBox
 from clang.cindex import Index, CursorKind
-from xml.etree import ElementTree
+from lxml import etree
 
 import platform
 if platform.system() == 'Windows':
     from clang.cindex import Config
     Config.set_library_path("C:\\Program Files\\LLVM\\bin")
-
-class CommentedTreeBuilder(ElementTree.TreeBuilder):
-    def __init__(self, factory = None):
-        ElementTree.TreeBuilder.__init__(self, factory)
-
-    def comment(self, data):
-        self.start(ElementTree.Comment, {})
-        self.data(data)
-        self.end(ElementTree.Comment)
 
 class MainWindow(QtWidgets.QWidget, Ui_Form):
     def __init__(self):
@@ -28,8 +19,9 @@ class MainWindow(QtWidgets.QWidget, Ui_Form):
         self.postfix = ''
         self.tree = None
         self.section = ''
-        self.child = None
+        self.pch = None
         self.willgen = {}
+        self.xml_file
 
     def insertRow(self, table, file, func):
         row = table.rowCount()
@@ -74,31 +66,30 @@ class MainWindow(QtWidgets.QWidget, Ui_Form):
             self.tableWidgetDest.removeRow(iitems[i].row())
 
     def openXmlFile(self):
-        file, ok = QFileDialog.getOpenFileName(self,
+        self.xml_file, ok = QFileDialog.getOpenFileName(self,
                                                  "选择cmd.xml文件",
                                                  "./",
                                                  "XML Files (*.xml)")
-        if file:
-            builder = CommentedTreeBuilder()
-            parser = ElementTree.XMLParser(target = builder, encoding='UTF-8')
-            self.tree = ElementTree.parse(file, parser = parser)
-            #ElementTree.dump(self.tree)
-            lprocess = self.tree.getiterator('process')
+        if self.xml_file:
+            self.tree = etree.parse(self.xml_file)
+            #etree.dump(self.tree.getroot())
+            lprocess = [el.text for pch in self.tree.getroot() for el in pch.iterchildren('process')]
             self.comboBoxSelSecXml.addItem("ALL")
-            self.comboBoxSelSecXml.addItems([process.text for process in lprocess])
+            self.comboBoxSelSecXml.addItems(lprocess)
 
     def selXmlSection(self, section):
         self.section = section
         if section == 'ALL':
-            self.textEditShow.setText(ElementTree.tostring(self.tree.getroot(), encoding='UTF-8').decode('UTF-8'))
+            self.textEditShow.setText(etree.tostring(self.tree, encoding='UTF-8', xml_declaration=True, pretty_print=True).decode('UTF-8'))
         else:
-            for child in self.tree.getroot():
-                if child[0].text == section:
-                    self.child = child
-                    self.textEditShow.setText(ElementTree.tostring(child, encoding='UTF-8').decode('UTF-8'))
+            for pch in self.tree.getroot():
+                if pch[0].text == section:
+                    self.pch = pch
+                    self.textEditShow.setText(etree.tostring(self.pch, encoding='UTF-8', pretty_print=True).decode('UTF-8'))
+                    break
 
     def genXmlText(self):
-        if not self.child:
+        if self.pch is None:
             QMessageBox.warning(self, "警告", "必须选择一个进程！", QMessageBox.Ok)
             return
         iitems = self.tableWidgetDest.selectedItems()
@@ -109,7 +100,7 @@ class MainWindow(QtWidgets.QWidget, Ui_Form):
                     self.willgen[str_file].append(self.tableWidgetDest.item(row, 1).text())
                 else:
                     self.willgen[str_file] = [self.tableWidgetDest.item(row, 1).text()]
-            print(self.willgen)
+            #print(self.willgen)
         else:
             for i in range(0, len(iitems), 2):
                 str_file = iitems[i].text()
@@ -117,10 +108,44 @@ class MainWindow(QtWidgets.QWidget, Ui_Form):
                     self.willgen[str_file].append(iitems[i+1].text())
                 else:
                     self.willgen[str_file] = [iitems[i+1].text()]
-            print(self.willgen)
+            #print(self.willgen)
+        num = int(list(self.pch.iterchildren('sourceFile'))[-1].attrib['num'])
+        for file_name, func_list in self.willgen.items():
+            num += 1
+            sourceFile = etree.SubElement(self.pch, "sourceFile", attrib={'num' : str(num)})
+            sourceFile.text = file_name + 'o' + '\n' + '\t'
+            sourceFile.tail = '\n'
+            comment = etree.Comment()
+            comment_str = '''\n\t补丁函数                             人员     时间        内容描述\n'''
+            func_list_str = ''
+            for func in func_list:
+                func_list_str += '\t' + func + self.postfix + '\n'
+            comment.text = comment_str + func_list_str + '\t'
+            comment.tail = '\n' + '\t'
+            sourceFile.append(comment)
+            for i in range(len(func_list)):
+                replace = etree.SubElement(sourceFile, "replace", attrib={'num' : str(i+1)})
+                replace.text = func_list[i] + self.postfix
+                replace.tail = ' '
+                replacedname = etree.SubElement(sourceFile, "replacedname")
+                replacedname.text = func_list[i]
+                replacedname.tail = ' '
+                replacedaddr = etree.SubElement(sourceFile, "replacedaddr")
+                replacedaddr.text = " "
+                replacedaddr.tail = '\n'
+                if i != len(func_list) - 1:
+                    replacedaddr.tail += '\t'
+        #etree.dump(self.pch)
+        self.textEditShow.setText(etree.tostring(self.pch, encoding='UTF-8', pretty_print=True).decode('UTF-8'))
 
     def saveXmlFile(self):
-        pass
+        pch = etree.fromstring(self.textEditShow.toPlainText())
+        pch.tail = '\n\n\n'
+        self.tree.getroot().replace(self.pch, pch)
+        #etree.dump(self.tree.getroot())
+        with open(self.xml_file, mode='w', encoding='UTF-8') as fd:
+            fd.write(etree.tostring(self.tree, encoding='UTF-8', pretty_print=True).decode('UTF-8'))
+        self.textEditShow.setDisabled(True)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
